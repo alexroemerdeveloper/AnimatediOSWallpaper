c9c4ce84c6c6700d588dcfcffae5ee76debc587f
Alexander Römer <alex@roemer.design>
2020-03-23 17:56:19 +0100

    Initial Commit

---
+++ AnimatediOSWallpaper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimatediOSWallpaper
+//
+//  Created by Alexander Römer on 23.03.20.
+//  Copyright © 2020 Alexander Römer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
